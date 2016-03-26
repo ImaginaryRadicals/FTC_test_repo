d9961cdaa865687086292653752ea8fe5308d4af
Ashley Trowell <ajtrowell@gmail.com>
2016-03-26 00:48:17 -0500

    Added pseudo-code for future dead-reckoning function

--- main.m
+++ main.m
@@ -1,10 +1,20 @@
 %% This is a pretend source file
 
 function [output] = main()
+% This will house the robot simulation timesteps
+
     display('Test output')
 
-
     output = sin(pi/2)
+
+    % Dead-reckoning robot function plan
+    % INPUTS: 
+        % robot setup structure:
+            % setup.wheelbase_cm = 30
+            % setup.wheel_radius_cm = 5
+        % right_wheel_rotation_radians
+        % left_wheel_rotation_radians
+    %OUTPUTS
 
 
 end
